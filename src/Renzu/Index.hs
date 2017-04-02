d6df20905cc6b109eb19bde922172adc09762d14
acple <silentsphere110@gmail.com>
2017-04-02 15:13:44 +0900

    inlining

--- src/Renzu/Index.hs
+++ src/Renzu/Index.hs
@@ -11,14 +11,19 @@ newtype Indexed i p s t = Indexed { runIndexed :: p (i, s) t }
 
 instance Profunctor p => Profunctor (Indexed i p) where
     dimap f g = Indexed . dimap (second' f) g . runIndexed
+    {-# INLINE dimap #-}
 
 instance Strong p => Strong (Indexed i p) where
     first'  = Indexed . lmap (\(i, (a, c)) -> ((i, a), c)) . first' . runIndexed
     second' = Indexed . lmap (\(i, (c, a)) -> (c, (i, a))) . second' . runIndexed
+    {-# INLINE first' #-}
+    {-# INLINE second' #-}
 
 instance Choice p => Choice (Indexed i p) where
     left'  = Indexed . lmap (\(i, ac) -> either (\a -> Left (i, a)) Right ac) . left' . runIndexed
     right' = Indexed . lmap (\(i, ca) -> either Left (\a -> Right (i, a)) ca) . right' . runIndexed
+    {-# INLINE left' #-}
+    {-# INLINE right' #-}
 
 ----------------------------------------------------------------
 
@@ -27,6 +32,8 @@ class Indexable i p q where
 
 instance Profunctor p => Indexable i p p where
     indexed = lmap snd
+    {-# INLINE indexed #-}
 
 instance i ~ j => Indexable i (Indexed j p) p where
     indexed = runIndexed
+    {-# INLINE indexed #-}

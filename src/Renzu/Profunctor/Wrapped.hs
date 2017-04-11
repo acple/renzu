48a2b4fd59ebabff5d3c168ec635bf0d75813b7c
acple <silentsphere110@gmail.com>
2017-04-11 17:36:03 +0900

    safe

--- src/Renzu/Profunctor/Wrapped.hs
+++ src/Renzu/Profunctor/Wrapped.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Renzu.Profunctor.Wrapped where
+module Renzu.Profunctor.Wrapped (Wrapped(..)) where
 
 ----------------------------------------------------------------
 
@@ -8,4 +7,25 @@ import Renzu.Profunctor.Class
 ----------------------------------------------------------------
 
 newtype Wrapped p a b = Wrap { unWrap :: p a b }
-    deriving (Profunctor, Strong, Choice)
+
+_map :: (p a b -> q s t) -> Wrapped p a b -> Wrapped q s t
+_map f = Wrap . f . unWrap
+{-# INLINE _map #-}
+
+instance Profunctor p => Profunctor (Wrapped p) where
+    dimap f g = _map (dimap f g)
+    {-# INLINE dimap #-}
+
+instance Strong p => Strong (Wrapped p) where
+    first = _map first
+    {-# INLINE first #-}
+
+    second = _map second
+    {-# INLINE second #-}
+
+instance Choice p => Choice (Wrapped p) where
+    left = _map left
+    {-# INLINE left #-}
+
+    right = _map right
+    {-# INLINE right #-}

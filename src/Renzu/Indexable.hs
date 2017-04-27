78e901b35781511a61ce948c2a753c66e3eca04c
acple <silentsphere110@gmail.com>
2017-04-27 19:36:56 +0900

    DefaultSignatures

--- src/Renzu/Indexable.hs
+++ src/Renzu/Indexable.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies, TypeFamilies #-}
+{-# LANGUAGE DefaultSignatures #-}
 module Renzu.Indexable where
 
 ----------------------------------------------------------------
@@ -10,27 +11,20 @@ import Renzu.Profunctor
 class Indexable i p q | p -> q where
     indexed :: p a b -> q (i, a) b
 
-instance Indexable i (->) (->) where
-    indexed = uncurry . const
-    {-# INLINE indexed #-}
-
-instance Indexable i (Forget r) (Forget r) where
+    default indexed :: Profunctor p => p a b -> p (i, a) b
     indexed = lmap snd
     {-# INLINE indexed #-}
 
-instance Profunctor p => Indexable i (Wrapped p) (Wrapped p) where
-    indexed = lmap snd
-    {-# INLINE indexed #-}
+instance Indexable i (->) (->)
 
-instance Indexable i (Capture a) (Capture a) where
-    indexed = lmap snd
-    {-# INLINE indexed #-}
+instance Indexable i (Forget r) (Forget r)
 
-instance Profunctor p => Indexable i (Re p s t) (Re p s t) where
-    indexed = lmap snd
-    {-# INLINE indexed #-}
+instance Indexable i (Capture a) (Capture a)
+
+instance Profunctor p => Indexable i (Wrapped p) (Wrapped p)
+
+instance Profunctor p => Indexable i (Re p s t) (Re p s t)
 
 instance i ~ j => Indexable i (Indexed j p) p where
     indexed = runIndexed
     {-# INLINE indexed #-}
-
